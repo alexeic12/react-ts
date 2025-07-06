b566ea2a1755c135f968931b8e08080326aa56ce
alexeic <alexeic@fpt.com>
2025-07-06 21:01:25 +0300

    restyle, and add more content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,8 +11,8 @@ export default function Home(): ReactElement {
         <h1 className={styles.title}>Hi, I'm Alex !</h1>
         <p className={styles.subtitle}>
           Welcome to my CV website. I'm a fullstack developer passionate about clean code,
-          beautiful design, and continuous learning.I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
-        </p>
+          beautiful design, and continuous learning.
+          </p>
         <div className={styles.buttons}>
           <button onClick={() => navigate("/projects")}>My Projects</button>
           <button onClick={() => navigate("/about")}>About Me</button>
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Projects.module.scss";
 
 export default function Projects(): ReactElement {
@@ -8,43 +8,83 @@ export default function Projects(): ReactElement {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Project 1</h1>
+        <h1 className={styles.title}>Casio Education</h1>
         <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
+        • Maintenance and optimization of cluster (K8S)
         </p>
-        <h1 className={styles.title}>Project 2</h1>
         <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
+        • FullStack development for new platform functionalities 
         </p>
-        <h1 className={styles.title}>Project 3</h1>
         <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
+        • Database administration
         </p>
-        <h1 className={styles.title}>Project 4</h1>
         <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
+        • Integration with Salesforce database
         </p>
-        <h1 className={styles.title}>Project 5</h1>
         <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
+        • Sanity Studio maintenance
         </p>
-        <h1 className={styles.title}>Project 5</h1>
         <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
-        </p>
-        <h1 className={styles.title}>Project 5</h1>
-        <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
-        </p>
-        <h1 className={styles.title}>Project 5</h1>
-        <p className={styles.subtitle}>
-          I am a creative person, with a strong intrinsic motivation and a constant curiosity towards new technological trends. I claim my confidence in using any new programming language and technological trend. I believe that adaptability is essential for success in the IT industry.
+        • Creating scripts for various tasks
         </p>
         <div className={styles.buttons}>
-          <button onClick={() => navigate("/projects")}>My Projects</button>
-          <button onClick={() => navigate("/")}>Home</button>
+          <button onClick={() => window.location.href = "https://casio-education.eu/"}>View &gt;</button>
         </div>
       </div>
-    </div>
+
+      <div className={styles.content}>
+      <h1 className={styles.title}>Evoload</h1>
+      <p className={styles.subtitle} onClick={() => window.location.href = "https://platform.evoload.co/login"}>
+      • Engineered the Freight Exchange platform at Evoload, enhancing logistics efficiency
+      <a href="https://platform.evoload.co/"> Visit&nbsp;&gt;</a>
+        </p>
+        <p className={styles.subtitle}>
+        • Spearheaded the development of the Evoload Staking platform, optimizing digital asset management.
+        <a href="https://staking.evoload.co/"> Visit&nbsp;&gt;</a>
+        </p>
+        <p className={styles.subtitle}>
+        • Innovated the Evoload Sales application to streamline pre-sale processes.
+        <a href="https://presale.evoload.co/"> Visit&nbsp;&gt;</a>
+        </p>
+        <p className={styles.subtitle}>
+        • Architected the Evoload Recruitment portal to automate talent acquisition .
+        <a href="https://portal.evoload.co/"> Visit&nbsp;&gt;</a>
+        </p>
+        <p className={styles.subtitle}>
+        • Crafted advanced asset analysis scripts, driving data-driven decision-making.
+        </p>
+        <p className={styles.subtitle}>
+        • Proficient in cutting-edge technologies: NodeJS, ReactJS, SocketIO, Sequelize, MySQL.
+        </p>
+        <p className={styles.subtitle}>
+        • Specialized in Smart Contract development, ensuring secure blockchain transactions.
+        </p>
+        <p className={styles.subtitle}>
+        • Integrated Web3 solutions, pioneering decentralized application connectivity.
+        </p>
+        <p className={styles.subtitle}>
+        • Designed robust Web3 project architectures, setting industry standards.
+        </p>
+        <p className={styles.subtitle}>
+        • Implemented Mobile App integration, delivering seamless cross-platform user experiences.
+        </p>
+
+        <div className={styles.buttons}>
+          <button onClick={() => window.location.href = "https://evoload.co/"}>View &gt;</button>
+        </div>
+</div>
+
+<div className={styles.content}>
+      <h1 className={styles.title}>SecureWorks</h1>
+        <p className={styles.subtitle}>
+        • Responsibilities included analyzing security incidents, network scenarios, identifying threats, creating rules for client's networks, and building scripts for automated tasks.
+        </p>
+</div>
+
+<div className={styles.buttonsCenter}>
+          <button onClick={() => navigate("/about")}>About Me</button>
+          <button onClick={() => navigate("/")}>Home</button>
+        </div>
+</div>
   );
 }
